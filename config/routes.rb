85a251f2162a010b108f24b351943d1c66c9d6b6
Marcial Pimentel <marzbar27@yahoo.com>
2017-11-21 14:44:52 -0600

    Got the Devise sign in as homepage

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :channels
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 
 end
