d374fbe1da4249f3a11dc63091867a950bb99083
Marcial Pimentel <marzbar27@yahoo.com>
2017-11-21 16:16:42 -0600

    Nav working, Devise sign in/out "working as intended."

--- app/controllers/User/sessions_controller.rb
+++ app/controllers/User/sessions_controller.rb
@@ -1,4 +1,5 @@
 class User::SessionsController < Devise::SessionsController
+  before_action :authenticate_user! ,:user_signed_in? ,:current_user ,:user_session
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   devise_for :users
   resources :comments
   resources :posts
   resources :channels
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+  root to: "home#index"
 
 end
