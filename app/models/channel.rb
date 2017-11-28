d4ef91c3f2abd711773b7ed4a60394705e0d1dc3
Marcial Pimentel <marzbar27@yahoo.com>
2017-11-28 15:36:22 -0600

    Added relationships back again

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,2 +1,5 @@
 class Channel < ApplicationRecord
+  belongs_to :user
+  has_many :posts
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  has_one :channel
+  belongs_to :user
+  has_many :comments
+
 end
