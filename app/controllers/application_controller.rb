87b431ff157e28737ddc85d56c4488d8b39e68df
marz <marcial00>
2017-12-01 20:38:58 -0600

    Merge branch 'master' of C:\Users\The Real Marzbar\IdeaProjects\SemesterProject with conflicts.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
-  def index
 
+
+  def index
+      @comments = Comment.where("user_id = ?", params[current_user])
   end
+
 end
